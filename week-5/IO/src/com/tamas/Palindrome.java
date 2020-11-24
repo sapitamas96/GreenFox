7640312eadb659adaf3772e521e79dd1327f881b
Tamas <sapitamas96@gmail.com>
2020-11-24 11:32:02 +0100

    Check for palindrome

--- week-5/IO/src/com/tamas/Palindrome.java
+++ week-5/IO/src/com/tamas/Palindrome.java
@@ -1,5 +1,7 @@
 package com.tamas;
 
+import java.util.ArrayList;
+
 public class Palindrome {
 
     public static void main(String[] args) {
@@ -8,6 +10,11 @@ public class Palindrome {
 
         System.out.println(createPalindrome("apple"));
         System.out.println(createPalindromeByFor("apple"));
+
+        //Készíts egy searchPalindrome nevű függvényt, ami követi a jelenlegi nyelv stílus útmutatóját.
+        // Bemeneten fogad egy stringet.
+        // Majd kikeresi, hogy taláhatóak-e 3 karakternél hoszabb palindrómák benne és végül visszaküldi a talált palindrómák listáját.
+        System.out.println(searchPalindrome("dog goat dad duck doodle never"));
     }
 
     public static String createPalindrome(String text) {
@@ -17,10 +24,28 @@ public class Palindrome {
 
     public static String createPalindromeByFor(String text) {
         String reverse = "";
-        String[] array = text.split("");
-        for (int i = array.length-1; i >= 0; i--) {
-            reverse += array[i];
+        for (int i = text.length()-1; i >= 0; i--) {
+            reverse += text.charAt(i);
         }
         return text + reverse;
+    }
+
+    public static ArrayList<String> searchPalindrome(String text) {
+        ArrayList<String> palindromes = new ArrayList<>();
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 3; j < text.length() - i; j++) {
+                String checkPalindrome = text.substring(i, i + j);
+                if (isPalindrome(checkPalindrome)) {
+                    palindromes.add(checkPalindrome);
+                }
+            }
+        }
+        
+        return palindromes;
+    }
+
+    public static boolean isPalindrome(String text) {
+        StringBuilder str = new StringBuilder(text);
+        return str.reverse().toString().equals(text);
     }
 }
